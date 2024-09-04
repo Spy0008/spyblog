22e23b906d141e41e2c5c1841c8588c585057fb7
spy0008 <namansompura2@gmail.com>
2024-09-04 15:22:07 +0530

    add signup functionality redirect to sign in page for sign in

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,46 @@
-import { Link } from 'react-router-dom';
-import { Button, Label, TextInput } from 'flowbite-react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { useState } from 'react';
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('please fill out all fields.')
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+
+      if (res.ok) {
+        return navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message)
+      setLoading(false);
+    }
+
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -15,16 +54,17 @@ export default function SignUp() {
             We can’t wait to welcome you! By signing up, you’ll gain access to all our articles, community discussions, and updates. Let’s share the journey!
           </p>
         </div>
-          {/* right */}
+        {/* right */}
 
         <div className="flex-1">
-          <form className='flex flex-col gap-3'>
+          <form className='flex flex-col gap-3' onSubmit={handleSubmit}>
             <div>
               <Label value='Enter Your username' />
               <TextInput
                 type='text'
                 placeholder='Username'
                 id='username'
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -33,17 +73,28 @@ export default function SignUp() {
                 type='email'
                 placeholder='Email'
                 id='email'
+                onChange={handleChange}
               />
             </div>
             <div>
-              <Label value='Enter Your password' />
+              <Label value='Enter Your password' x />
               <TextInput
                 type='password'
                 placeholder='Password'
                 id='password'
+                onChange={handleChange}
               />
             </div>
-            <Button gradientDuoTone='pinkToOrange' type='submit'>Sign Up</Button>
+            <Button disabled={loading} className='active:scale-95 disabled:opacity-70' gradientDuoTone='pinkToOrange' type='submit'>
+              {
+                loading ? (
+                  <>
+                    <Spinner size='sm' />
+                    <span className='pl-3'>Loading...</span>
+                  </>
+                ) : 'Sign Up'
+              }
+            </Button>
           </form>
           <div className='flex gap-2 text-sm mt-3'>
             <span>Already have an account?</span>
@@ -51,6 +102,13 @@ export default function SignUp() {
               Sign In
             </Link>
           </div>
+          {
+            errorMessage && (
+              <Alert className='mt-5' color='failure'>
+                {errorMessage}
+              </Alert>
+            )
+          }
         </div>
       </div>
     </div>
