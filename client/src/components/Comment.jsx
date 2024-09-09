639321387da5b58f4d88506a2cbe88c42897b3bf
spy0008 <namansompura2@gmail.com>
2024-09-09 19:05:15 +0530

    add like button on comment section

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,37 +1,49 @@
 import { useEffect, useState } from "react"
 import moment from 'moment';
+import { FaThumbsUp } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
-export default function Comment({ comment }) {
-    const [user, setUser] = useState({});
-    console.log(user)
-    useEffect(() => {
-        const getUser = async () => {
-          try {
-            const res = await fetch(`/api/user/${comment.userId}`);
-            const data = await res.json();
-            if (res.ok) {
-              setUser(data);
-            }
-          } catch (error) {
-            console.log(error.message);
-          }
-        };
-        getUser();
-      }, [comment]);
-    return(
-        <div className="flex p-4 border-b dark:border-zinc-700 text-sm">
-            <div className="flex-shrink-0 mr-3">
-                <img className="w-10 h-10 rounded-full bg-zinc-200" src={user.profilePicture} alt={user.username} />
-            </div>
-            <div className="flex-1">
-                <div className="flex items-center mb-1">
-                    <span className="font-bold mr-1 text-xs truncate">{user ? `@${user.username}` : "unkonwn user"}</span>
-                    <span className="text-zinc-600 text-xs">
-                        {moment(comment.createdAt).fromNow()}
-                    </span>
-                </div>
-                <p className="text-zinc-600 mb-2">{comment.content}</p>
-            </div>
+export default function Comment({ comment, onLike }) {
+  const { currentUser } = useSelector((state) => state.user)
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await fetch(`/api/user/${comment.userId}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUser(data);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getUser();
+  }, [comment]);
+  return (
+    <div className="flex p-4 border-b dark:border-zinc-700 text-sm">
+      <div className="flex-shrink-0 mr-3">
+        <img className="w-10 h-10 rounded-full bg-zinc-200" src={user.profilePicture} alt={user.username} />
+      </div>
+      <div className="flex-1">
+        <div className="flex items-center mb-1">
+          <span className="font-bold mr-1 text-xs truncate">{user ? `@${user.username}` : "unkonwn user"}</span>
+          <span className="text-zinc-600 text-xs">
+            {moment(comment.createdAt).fromNow()}
+          </span>
         </div>
-    )
+        <p className="text-zinc-600 mb-2 dark:text-zinc-100">{comment.content}</p>
+        <div className="flex items-center pt-2 text-sm border-t dark:border-zinc-700 max-w-fit gap-2">
+          <button type="button" onClick={() => onLike(comment._id)} className={`text-gray-400 ${currentUser && comment.likes.includes(currentUser._id) && '!text-orange-500'}`}>
+            <FaThumbsUp className="text-sm"  />
+          </button>
+          <p className="text-zinc-600">
+            {
+              comment.numberOfLikes > 0 && comment.numberOfLikes +  " " + (comment.numberOfLikes === 1 ? "like" : "likes")
+            }
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
